46eb06b97fb1d1c6d7834692c18f788ce1274f2e
Stephanie BeauzileD <stephanie.beauzile.d@gmail.com>
2020-05-12 19:35:58 -0400

    start to work on the 'add new task'

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,18 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new # render the from
+    @task = Task.new
+  end
+
+  def create
+    new_task = Task.create(params[:tasks])
+
+    redirect_to task_path(new_task)
+  end
 end
